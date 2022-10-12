2eb0e207dc34243f60e291cde2e062484196490f
2olaf2 <rivasolaf250@gmail.com>
2022-10-12 11:20:04 -0500

    clase 19 mobile menu

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuBuger = document.querySelector(".menu");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+menuBuger.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu(){
   desktopMenu.classList.toggle("inactive");
+}
+function toggleMobileMenu(){
+  mobileMenu.classList.toggle("inactive");
 }
